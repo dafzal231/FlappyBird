8a7ebdabe33d7fbc9775cb29e2153521a8abbb4c
Muhammad Danyal Afzal <67021862+dafzal231@users.noreply.github.com>
2022-10-14 15:04:50 -0400

    project

---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    
+    public GameObject prefab;
+
+    public float spawnRate = 1f;
+    public float minHeight = -1f;
+    public float maxHeight = 2f;
+
+    private void OnEnable(){
+        InvokeRepeating("Spawn", spawnRate, spawnRate);
+    }
+
+    private void OnDisable(){
+        CancelInvoke("Spawn");
+    }
+
+    private void Spawn(){
+        GameObject Pipes = Instantiate(prefab, transform.position, Quaternion.identity);
+        Pipes.transform.position += Vector3.up * Random.Range(minHeight, maxHeight);
+    }
+
+}
